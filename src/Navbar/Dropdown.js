67741a4d6fd06eb7216e7b31cac1edc8abcf925b
Zeeshan <zeeshan.sheikh@easyeat.ai>
2022-08-01 16:29:45 +0530

    separate Navbar component

---
+++ src/Navbar/Dropdown.js
@@ -0,0 +1,73 @@
+import React from "react";
+import { BiChevronDown } from "react-icons/bi";
+function Dropdown() {
+  return (
+    <>
+      <ul>
+        <li>
+          <a href="#">
+            Company <BiChevronDown />
+          </a>
+          <div className="sub-menu-2">
+            <ul>
+              <li>
+                <a href="#">About</a>
+              </li>
+              <li>
+                <a href="#">Careers</a>
+              </li>
+              <li>
+                <a href="#">Newsroom</a>
+              </li>
+            </ul>
+          </div>
+        </li>
+        <li>
+          {" "}
+          <a href="#">
+            Resources
+            <BiChevronDown />
+          </a>
+          <div className="sub-menu-2">
+            <ul>
+              <li>
+                <a href="#">All Resources</a>
+              </li>
+              <li>
+                <a href="#">Blog</a>
+              </li>
+              <li>
+                <a href="#">Case Studies</a>
+              </li>
+              <li>
+                <a href="#">Testimonials</a>
+              </li>
+              <li>
+                <a href="#">Product Newsletter-Nucleus</a>
+              </li>
+            </ul>
+          </div>
+        </li>
+        <li>
+          {" "}
+          <a href="#">Integration</a>
+        </li>
+        <li>
+          {" "}
+          <a href="#">Social Impact</a>
+        </li>
+        <li>
+          <a href="#">Support</a>
+        </li>
+        <li>
+          <a href="#">Contact Sales</a>
+        </li>
+        <li>
+          <a href="#">Login</a>
+        </li>
+      </ul>
+    </>
+  );
+}
+
+export default Dropdown;
---
+++ src/Navbar/Menu.js
@@ -0,0 +1,112 @@
+import React, { useState } from "react";
+import "./Navbar.css";
+import logo from "../Images/freight-tiger-logo.png";
+import icon1 from "../Images/ft-visibility.svg";
+import icon2 from "../Images/ft-tms.svg";
+import icon3 from "../Images/ft-dfn.svg";
+
+function Menu() {
+  const [showButton, toggleClick] = useState(false);
+  const [showButton2, toggleClick2] = useState(false);
+  const [showButton3, toggleClick3] = useState(false);
+
+  const handleClick = (event) => {
+    toggleClick((prev) => !prev);
+
+    toggleClick2(false);
+    toggleClick3(false);
+  };
+  const handleClick1 = (event) => {
+    toggleClick(false);
+    toggleClick2((prev) => !prev);
+    toggleClick3(false);
+  };
+  const handleClick2 = (event) => {
+    toggleClick(false);
+    toggleClick2(false);
+    toggleClick3((prev) => !prev);
+  };
+  return (
+    <div>
+      <div className="visible-icon">
+        <img src={icon1} className="app-icon1" alt="logo" />
+        <div className="styles_text">
+          <a href="#">FT Visibility</a>
+          <div className="styles-text-para">
+            <p>
+              Get complete visibility of movement of your trucks and material,
+              easily, and all in one place
+            </p>
+          </div>
+        </div>
+      </div>
+      <div className="styles_divider_"></div>
+      <div className="visible-icon">
+        <img src={icon2} className="app-icon1" alt="logo" />
+        <div className="styles_text">
+          <a href="#">FT TMS</a>
+          <div className="styles-text-para">
+            <p>
+              360º management of your freight operations, from procurement to
+              delivery and billing.
+            </p>
+          </div>
+        </div>
+      </div>
+      <div className="styles_divider_"></div>
+      <div className="visible-icon">
+        <img src={icon3} className="app-icon1" alt="logo" />
+        <div className="styles_text">
+          {" "}
+          <a href="#">FT Digital Freight Network</a>
+          <div className="styles-text-para">
+            <p>
+              Matching real time demand to real time access to 4k+ LSPs in the
+              most transparent and neutral manner.
+            </p>
+          </div>
+        </div>
+      </div>
+      <div className="styles_divider_"></div>
+      <div className="styles_text">
+        <a href="#" onClick={handleClick}>
+          For LSPs
+        </a>
+        {showButton ? (
+          <ul className="ul_text">
+            <li>FT Visibility for LSPs</li>
+            <li>FT Freight Network for LSPs</li>
+          </ul>
+        ) : null}
+      </div>
+      <div className="styles_divider_"></div>
+      <div className="styles_text">
+        <a href="#" onClick={handleClick1}>
+          Company
+        </a>
+        {showButton2 ? (
+          <ul className="ul_text">
+            <li>About</li>
+            <li>Careers</li>
+            <li>Newsroom</li>
+          </ul>
+        ) : null}
+      </div>
+      <div className="styles_divider_"></div>
+      <div className="styles_text">
+        <a href="#" onClick={handleClick2}>
+          Resources
+        </a>
+        {showButton3 ? (
+          <ul className="ul_text">
+            <li>About</li>
+            <li>Careers</li>
+            <li>Newsroom</li>
+          </ul>
+        ) : null}
+      </div>
+    </div>
+  );
+}
+
+export default Menu;
