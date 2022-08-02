49f70aee82d4965b0dea4e24e1b50ad542787bb2
Zeeshan <zeeshan.sheikh@easyeat.ai>
2022-08-03 02:55:23 +0530

    JSON- Separate

--- src/Navbar/Dropdown.js
+++ src/Navbar/Dropdown.js
@@ -1,68 +1,60 @@
 import React from "react";
 import { BiChevronDown } from "react-icons/bi";
+import arrowIcon from "../Images/arrow-icon.svg";
 function Dropdown() {
+  const dropdownItems = [
+    {
+      item: "Company",
+      href: "#",
+      subItems: [
+        { name: "About", href: "#" },
+        { name: "Careers", href: "#" },
+        { name: "Newsroom", href: "#" },
+      ],
+    },
+    {
+      item: "Resources",
+      href: "#",
+      subItems: [
+        { name: "All Resources", href: "#" },
+        { name: "Blogs", href: "#" },
+        { name: "Case Studies", href: "#" },
+        { name: "Testimonials", href: "#" },
+        { name: "Product Newsletter-Nucleus", href: "#" },
+      ],
+    },
+    { item: "Integration", href: "#" },
+    { item: "Social Impact", href: "#" },
+    { item: "Support", href: "#" },
+    { item: "Contact Sales", href: "#" },
+    { item: "Login", href: "#" },
+  ];
   return (
     <>
-      <ul>
-        <li>
-          <a href="#">
-            Company <BiChevronDown />
-          </a>
-          <div className="sub-menu-2">
-            <ul>
-              <li>
-                <a href="#">About</a>
-              </li>
-              <li>
-                <a href="#">Careers</a>
-              </li>
-              <li>
-                <a href="#">Newsroom</a>
-              </li>
-            </ul>
-          </div>
-        </li>
-        <li>
-          <a href="#">
-            Resources
-            <BiChevronDown />
-          </a>
-          <div className="sub-menu-2">
-            <ul>
-              <li>
-                <a href="#">All Resources</a>
-              </li>
-              <li>
-                <a href="#">Blog</a>
-              </li>
-              <li>
-                <a href="#">Case Studies</a>
-              </li>
-              <li>
-                <a href="#">Testimonials</a>
-              </li>
-              <li>
-                <a href="#">Product Newsletter-Nucleus</a>
-              </li>
-            </ul>
-          </div>
-        </li>
-        <li>
-          <a href="#">Integration</a>
-        </li>
-        <li>
-          <a href="#">Social Impact</a>
-        </li>
-        <li>
-          <a href="#">Support</a>
-        </li>
-        <li>
-          <a href="#">Contact Sales</a>
-        </li>
-        <li>
-          <a href="#">Login</a>
-        </li>
-      </ul>
+    <div className="oulet-container">
+      {dropdownItems.map((menuItem) => {
+        return (
+          <ul>
+            <li>
+              <a href={menuItem.href} >{menuItem.item}{menuItem.subItems && <BiChevronDown />} </a>
+              <div className="sub-menu-2">
+                {menuItem.subItems &&
+                  menuItem.subItems.map((subItem) => {
+                    return (
+                      <ul>
+                        <li>
+                          <a href={subItem.href}>{subItem.name}</a>
+                        </li>
+                         
+                      </ul>
+                    );
+                  })}
+              </div>
+            </li>
+          </ul>
+        );
+      })}
+      </div>
     </>
   );
 }
